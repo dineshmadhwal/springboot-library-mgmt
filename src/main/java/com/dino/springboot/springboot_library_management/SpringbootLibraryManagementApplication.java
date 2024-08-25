08558219c87f1cf39d261c0cd9efb4be9d2002f2
Dinesh Madhwal <dineshmadhwal@gmail.com>
2024-08-25 14:02:07 +0800

    First Commit

---
+++ src/main/java/com/dino/springboot/springboot_library_management/SpringbootLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dino.springboot.springboot_library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLibraryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dino/springboot/springboot_library_management/SpringbootLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dino.springboot.springboot_library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
